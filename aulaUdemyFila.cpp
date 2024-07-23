d7e5256eda0e0b75d0c2b23c84804ae216d85433
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2024-07-23 11:14:28 -0300

    Desenfileirar, Tamanho, Vazia ou cheia

--- aulaUdemyFila.cpp
+++ aulaUdemyFila.cpp
@@ -19,15 +19,41 @@ void fila_construtor(int *frente,int *tras){
     *tras = -1;
 }
 
+bool fila_vazia(int frente, int tras){
+    if(frente > tras){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+bool fila_cheia(int tras){
+    if(tras == TAM - 1){
+        return true;
+    }
+    return false;
+}
+
 void fila_enfileirar(int fila[TAM], int valor, int *tras){
 
-    if(*tras == TAM -1){
+    if(fila_cheia(*tras)){
         cout<<"fila cheia!";
     }else{
-        *tras += 1;
+        *tras = *tras +  1;
         fila[*tras] = valor;
     }
 }
+
+void fila_desenfileirar(int fila[TAM],int *frente,int tras){
+    if(!fila_vazia(*frente,tras)){
+    cout<<"O valor "<< fila[*frente]<<" foi removido | ";
+    fila[*frente] = 0;
+    *frente = *frente + 1;
+    }else{
+        cout << "Imposivel desinfileirar uma lista vazia | ";
+    }
+}
+
 
 int fila_tamanho(int frente,int tras){
     return(tras - frente) + 1;
@@ -38,24 +64,25 @@ int main()
 
     int fila[TAM] = {0,0,0,0,0,0,0,0,0,0};
     int frente,tras;
-    int valor;
+    int valor, valorRetirado;
 
     fila_construtor(&frente, &tras);
 
     fila_enfileirar(fila, 5, &tras);
     fila_enfileirar(fila, 7, &tras);
-    fila_enfileirar(fila, 8, &tras);
-    fila_enfileirar(fila, 8, &tras);
-    fila_enfileirar(fila, 8, &tras);
-    fila_enfileirar(fila, 8, &tras);
-    fila_enfileirar(fila, 8, &tras);
-    fila_enfileirar(fila, 8, &tras);
-    fila_enfileirar(fila, 8, &tras);
-    fila_enfileirar(fila, 8, &tras);
-    fila_enfileirar(fila, 8, &tras);
-    fila_enfileirar(fila, 8, &tras);
+    fila_enfileirar(fila, 9, &tras);
 
     imprimir_vetor(fila, fila_tamanho(tras, frente));
+
+    fila_desenfileirar(fila,&frente,tras);
+    fila_desenfileirar(fila,&frente,tras);
+    fila_desenfileirar(fila,&frente,tras);
+    fila_desenfileirar(fila,&frente,tras);
+    fila_desenfileirar(fila,&frente,tras);
+
+    imprimir_vetor(fila, fila_tamanho(tras, frente));
+
+
 
     return 0;
 }
