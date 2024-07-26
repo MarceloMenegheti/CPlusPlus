37c859c57cb48741471d0c9c24befaeacdd837ca
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2024-07-26 12:43:19 -0300

    Insercao no inicio da Lista Sequencial

--- aulaUdemyMenuListaSequencial.cpp
+++ aulaUdemyMenuListaSequencial.cpp
@@ -5,17 +5,89 @@
 
 using namespace std;
 
+struct pessoa{
+    string nome;
+    int rg;
+};
+
 void limpaTela(){
     system("CLS");
+}
+
+void imprimirSequencial(pessoa *ponteiroSequencial, int tamanhoDaLista){
+
+    for(int i = 0; i < tamanhoDaLista; i++){
+        cout<<i<<" - "<<ponteiroSequencial[i].nome <<", "<< ponteiroSequencial[i].rg<<endl;
+    }
+}
+
+void addComecoSequencial(string nome, int rg, int *tamanhoDaLista, pessoa *&ponteiroSequencial){
+
+    if(*tamanhoDaLista == 0){
+
+        //caso a lista ja tenha membros
+        pessoa *novaListaSequencial = new pessoa[1];
+
+        //inserir o primeiro membros
+        novaListaSequencial[0].nome = nome;
+        novaListaSequencial[0].rg = rg;
+
+        //atualiza o ponteiro para a lista nova
+        ponteiroSequencial = novaListaSequencial;
+
+    }else{
+
+        //caso o vetor ja tenha membros
+        pessoa *novaListaSequencial = new pessoa[*tamanhoDaLista + 1];
+
+        //insere o primeiro novo elemento
+        novaListaSequencial[0].nome = nome;
+        novaListaSequencial[0].rg = rg;
+
+        //passa os elementos para o vetor antigo para o novo
+        for(int i = 0; i < *tamanhoDaLista; i++){
+            novaListaSequencial[i + 1].nome = ponteiroSequencial[i].nome;
+            novaListaSequencial[i + 1].rg = ponteiroSequencial[i].rg;
+        }
+
+        //atualiza o ponteiro para a lista nova
+        ponteiroSequencial = novaListaSequencial;
+    }
+
+    //aumenta o tamanho da lista
+    *tamanhoDaLista = *tamanhoDaLista + 1;
 }
 
 int main(){
 
     //variaveis
     int funcaoDesejada = 1;
-limpaTela();
+
+    //ponteiro para lista sequecial
+    pessoa *ponteiroSequecial;
+
+    //tamanho da lista
+    int tamanhoDaLista = 0;
+
+    //exemplo
+    pessoa *exemploListaSequecial = new pessoa[tamanhoDaLista];
+
+    /*//dois registros de exemplo
+    exemploListaSequecial[0].nome = "John";
+    exemploListaSequecial[0].rg = 178;
+
+    exemploListaSequecial[1].nome = "Bia";
+    exemploListaSequecial[1].rg = 476;*/
+
+    //faz o ponteiro principal apontar para o novo vetor
+    ponteiroSequecial = exemploListaSequecial;
+
+
+    //mostrando o menu
     while(funcaoDesejada < 10 && funcaoDesejada > 0){
 
+        //imprimi a lista completa
+        imprimirSequencial(ponteiroSequecial, tamanhoDaLista);
 
         cout<<" Operacoes"<<endl;
         cout<<"1 - insercao de um node no inicio da lista"<<endl;
@@ -31,10 +103,23 @@ limpaTela();
 
         cin>>funcaoDesejada;
 
+        limpaTela();
 
+        //variaveis usadas nas operacoes
+        string nome;
+        int rg;
+
+        //chamando a funcao desejada
         switch(funcaoDesejada){
             case 1:
-                cout<<"Funcao escolhida: 1"<<endl;
+                cout<<"Funcao escolhida: 1 - insercao de um node no inicio da lista"<<endl;
+                cout<<"Digite o nome: ";
+                cin>>nome;
+
+                cout<<"Digite o rg: ";
+                cin>>rg;
+
+                addComecoSequencial(nome,rg, &tamanhoDaLista,ponteiroSequecial);
                 break;
             case 2:
                 cout<<"Funcao escolhida: 2"<<endl;
@@ -61,10 +146,7 @@ limpaTela();
                 cout<<"Funcao escolhida: 9"<<endl;
                 break;
         }
-
     }
-
-
 
     return 0;
 }
