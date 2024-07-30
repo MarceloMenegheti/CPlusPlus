362c1567537ba5b5b0b106a9a7bc642832cd1704
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2024-07-30 12:34:36 -0300

    Estrutura de Pessoa e Lista Encadeada inicial

--- aulaUdemyMenuListaEncadeada.cpp
+++ aulaUdemyMenuListaEncadeada.cpp
@@ -1,45 +1,124 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+#include <string>
 #include <new>
 
+using namespace std;
 
-struct estrutura{
-    int valorNumerico;
-    struct estrutura *proximo;
+struct pessoa{
+    string nome;
+    int rg;
+    pessoa *proximo;
 };
+
+void limpaTela(){
+    system("CLS");
+}
+
+int retornaTamanho(pessoa *ponteiroEncadeado){
+
+    //tamanho da lista
+    int tamanho = 0;
+
+    //Ponteiro cursor auxiliar
+    pessoa *p = ponteiroEncadeado;
+
+    while(p != NULL){
+
+        //atualiza o cursor
+        p = p->proximo;
+
+        //contador de tamanho aumenta
+        tamanho++;
+    }
+    return tamanho;
+
+}
+
+void imprimirEncadeado(pessoa *ponteiroEncadeado){
+
+    //Ponteiro cursor auxiliar
+    pessoa *p = ponteiroEncadeado;
+
+        while(p != NULL){
+
+            //imprimi a lista
+            cout<< p->nome << ", "<< p->rg <<endl;
+
+            //atualiza o cursor
+            p = p->proximo;
+
+    }
+}
+
 
 int main(){
 
-    //cria o inicio da lista
-    estrutura *ponteiroEncadeado;
+    //variaveis
+    int funcaoDesejada = 1;
 
-    //cria um novo valor
-    estrutura *novoPrimeiroValor = new estrutura;
-    novoPrimeiroValor->valorNumerico = 13;
+    //ponteiro para lista encadeada
+    pessoa *ponteiroEncadeado;
+
+    //cria o primeiro valor
+    pessoa *novoPrimeiroValor = new pessoa;
+    novoPrimeiroValor->nome = "Marcelo";
+    novoPrimeiroValor->rg = 666;
     novoPrimeiroValor->proximo = NULL;
 
-    //faz o ponteiro da encadeada apontar para o primeiro valor
+    //Transforma o ponteiro do inicio da lista no novo valor
     ponteiroEncadeado = novoPrimeiroValor;
 
-    //cria um segundo valor
-    estrutura *novoSegundoValor = new estrutura;
-    novoSegundoValor->valorNumerico = 20;
+    //cria o novo Segundo valor
+    pessoa *novoSegundoValor = new pessoa;
+    novoSegundoValor->nome = "Diego";
+    novoSegundoValor->rg = 111;
     novoSegundoValor->proximo = NULL;
 
-    //junta os valores com um encadeamento
-    ponteiroEncadeado->proximo = novoSegundoValor;
+    //Encadeamento de valores
+    novoPrimeiroValor->proximo = novoSegundoValor;
 
-    //imprimi a lista encadeada
-    while(ponteiroEncadeado != NULL){
 
-        //imprimi o valor atual
-        printf("%nValor: %d",ponteiroEncadeado->valorNumerico);
+    while(funcaoDesejada < 9 && funcaoDesejada > 0){
 
-        //cursor recebe a posicao de memoria do proximo valor
-        ponteiroEncadeado = ponteiroEncadeado->proximo;
+        //mostrando o menu
 
+        cout<<" Operacoes"<<endl;
+        cout<<"1 - insercao de um node no inicio da lista"<<endl;
+        cout<<"2 - insercao de um node no fim da lista"<<endl;
+        cout<<"3 - insercao de um node na posicao"<<endl;
+        cout<<"4 - retirar um node no inicio da lista"<<endl;
+        cout<<"5 - retirar um node no fim da lista"<<endl;
+        cout<<"6 - retirar um node na posicao"<<endl;
+        cout<<"7 - procurar um node com o campo RG"<<endl;
+        cout<<"8 - imprimir a lista. "<<endl;
+        cout<<"9 - sair do sistema. "<<endl;
+
+        cout<<"\nTamanho Atual: "<<retornaTamanho(ponteiroEncadeado)<<endl;
+
+        imprimirEncadeado(ponteiroEncadeado);
+
+        cout<<"\nEscolha um numero e pressione ENTER: ";
+
+        cin>>funcaoDesejada;
+
+        limpaTela();
+
+        //variaveis usadas nas operacoes
+        string nome;
+        int rg,posicao;
+
+        //chamando a funcao desejada
+        switch(funcaoDesejada){
+            case 1:
+                cout<<""<<endl;
+                break;
+            case 2:
+                cout<<"!"<<endl;
+                break;
+        }
     }
-
 
 return 0;
 }
