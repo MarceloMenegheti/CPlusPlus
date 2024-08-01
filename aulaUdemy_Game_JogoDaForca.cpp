9a8228521d47e742e8984f251237f678ddcf4877
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2024-07-31 21:30:44 -0300

    Incluindo as palavras Aleatorias

--- aulaUdemy_Game_JogoDaForca.cpp
+++ aulaUdemy_Game_JogoDaForca.cpp
@@ -2,13 +2,33 @@
 #include <stdlib.h>
 #include <string>
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
 void limpaTela(){
-
 system("CLS");
+}
 
+string retornaPalavraAleatoria(){
+
+    //Vetor com palavras disponíveis
+    string palavras[4] = {"Abacaxi","Manga","Morango","Jabuticaba"};
+
+    //Indice gerado no intervalo {0,1,2,3}
+    int indiceAleatorio = rand() % 4;
+
+    //Exibe a palavra aleatória
+    //cout << palavras[indiceAleatorio];
+
+    return palavras[indiceAleatorio];
+}
+
+void jogarSozinho(){
+
+    string palavra = retornaPalavraAleatoria();
+
+    cout<<"A palavra secreta eh "<<palavra<<endl;
 }
 
 void menuInicial(){
@@ -31,7 +51,8 @@ void menuInicial(){
         switch(opcao){
 
         case 1:
-            cout<<"\nJogo iniciado"<<endl;
+            cout<<"\nJogo iniciado\n"<<endl;
+            jogarSozinho();
             break;
 
         case 2:
@@ -45,9 +66,14 @@ void menuInicial(){
         }
     }
 }
+
+
 int main(){
 
-menuInicial();
+
+    srand( (unsigned)time(NULL));
+
+    menuInicial();
 
     return 0;
 }
