36b544470e0c21ba4e226ecb9960fd86d5a10a67
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2024-08-06 15:25:48 -0300

    Conclusao do jogo

--- aulaUdemy_Game_JogoDaForca.cpp
+++ aulaUdemy_Game_JogoDaForca.cpp
@@ -7,16 +7,16 @@
 using namespace std;
 
 void limpaTela(){
-system("CLS");
+    system("CLS");
 }
 
 string retornaPalavraAleatoria(){
 
-    //Vetor com palavras disponíveis
-    string palavras[4] = {"abacaxi","manga","morango","jabuticaba"};
+     //Vetor com palavras disponíveis
+    string palavras[3] = {"abacaxi", "manga", "morango"};
 
-    //Indice gerado no intervalo {0,1,2,3}
-    int indiceAleatorio = rand() % 4;
+    //Indice gerado no intervalo {0,1,2}
+    int indiceAleatorio = rand() % 3;
 
     //Exibe a palavra aleatória
     //cout << palavras[indiceAleatorio];
@@ -46,27 +46,41 @@ void exibeStatus(string palavraComMascara, int tamanhoDaPalavra, int tentativasR
     cout << "\nTentativas Restantes:" << tentativasRestantes;
 
     //Exibe as letras arriscadas
+    int cont;
     cout << "\nLetras arriscadas:";
-    for(int i = 0; i < letrasJaArriscadas.size(); i++){
-        cout << letrasJaArriscadas[i] << ", ";
+    for(cont = 0; cont < letrasJaArriscadas.size();cont++){
+        cout << letrasJaArriscadas[cont] << ", ";
     }
 
 }
 
-void jogarSozinho(){
+int jogar(int numeroDeJogadores){
 
-    //palavra a ser adivinhada
-    string palavra = retornaPalavraAleatoria();
+    //Palavra a ser adivinhada
+    string palavra;
 
-    //tamanho da palavra
+    //Confere o número de jogadores
+    if(numeroDeJogadores == 1){
+
+        //Palavra a ser adivinhada
+        palavra = retornaPalavraAleatoria();
+
+    }else{
+
+        cout << "\nDigite uma palavra:";
+        cin >> palavra;
+
+    }
+
+    //Tamanho da palavra
     int tamanhoDaPalavra = palavra.size();
 
-    //palavra mascarada
-    string palavraComMascara = retornaPalavraComMascara(palavra,tamanhoDaPalavra);
+    //Palavra mascarada
+    string palavraComMascara = retornaPalavraComMascara(palavra, tamanhoDaPalavra);
 
-     ///Variáveis Gerais
-    int tentativas = 0, maxTentativas = 10;            //Número de tentativas e erros
-    //int i = 0;                                            //Auxiliar para laços de repetição
+    ///Variáveis Gerais
+    int tentativas = 0, maximoDeTentativas = 10;            //Número de tentativas e erros
+    int cont = 0;                                           //Auxiliar para laços de repetição
     char letra;                                             //Letra arriscada pelo usuário
     int opcao;                                              //Opções finais
     string letrasJaArriscadas;                              //Acumula as tentativas do jogador
@@ -74,16 +88,17 @@ void jogarSozinho(){
     string palavraArriscada;                                //Tentativa de arriscar a palavra completa
     bool jaDigitouLetra = false, acertouLetra = false;      //Auxiliar para saber se a letra já foi digitada
 
-    while(palavra != palavraComMascara && maxTentativas - tentativas > 0){
+
+    while(palavra != palavraComMascara && maximoDeTentativas - tentativas > 0){
 
         limpaTela();
 
-        //exibe o Status Atual do jogo
-        exibeStatus(palavraComMascara, tamanhoDaPalavra, maxTentativas - tentativas, letrasJaArriscadas,mensagem);
+        //Exibe o status atual do jogo
+        exibeStatus(palavraComMascara, tamanhoDaPalavra, maximoDeTentativas - tentativas, letrasJaArriscadas,mensagem);
 
-        //de um palpite
-        cout<<"\nDigite uma letra (ou Digite 1 para arriscar a palavra): ";
-        cin>>letra;
+        //Lê um palpite
+        cout << "\nDigite uma letra (Ou digite 1 para arriscar a palavra):";
+        cin >> letra;
 
         //Se digitar 1 deixa o usuário arriscar a palavra inteira
         if(letra == '1'){
@@ -91,17 +106,17 @@ void jogarSozinho(){
             if(palavraArriscada == palavra){
                  palavraComMascara = palavraArriscada;
             }else{
-                 tentativas = maxTentativas;
+                 tentativas = maximoDeTentativas;
             }
         }
 
-        //percorre as letras ja digitadas
-        for(int i = 0; i < tentativas; i++){
+        //Percorre as letras já arriscadas
+        for(cont = 0; cont < tentativas; cont++){
 
-            //se encontrar a letra
-            if(letrasJaArriscadas[i] == letra){
+            //Se encontrar a letra
+            if(letrasJaArriscadas[cont] == letra){
 
-                mensagem = "\nEssa letra ja foi digitada\n";
+                mensagem = "Essa letra ja foi digitada!";
 
                 //Indica com a variável booleana
                 jaDigitouLetra = true;
@@ -109,36 +124,39 @@ void jogarSozinho(){
             }
         }
 
-        //se for uma letra nova
+        //Se for uma letra nova
         if(jaDigitouLetra == false){
 
             //Incrementa as letras já arriscadas
-            letrasJaArriscadas += letra;
+            letrasJaArriscadas += tolower(letra);
 
-            //percorre a palavra real
-            for(int i = 0; i < tamanhoDaPalavra; i++){
+            //Percorre a palavra real e
+            for(cont = 0; cont < tamanhoDaPalavra; cont++){
 
-                //se a letra exitir  na palavra escondida
-                if(palavra[i] == letra){
+                //Se a letra existir na palavra escondida
+                if(palavra[cont] == tolower(letra)){
 
-                    //faco aquela letra aparecer na palavraComMascara
-                    palavraComMascara[i] = palavra[i];
-
+                    //Faço aquela letra aparecer na palavraComMascara
+                    palavraComMascara[cont] = palavra[cont];
 
                     acertouLetra = true;
+
                 }
             }
 
-            if(!acertouLetra){
-                mensagem = "Voce ERROU";
+            if(acertouLetra == false){
+
+                mensagem = "Voce errou uma letra!";
 
             }else{
-                mensagem = "\nVoce acertou uma letra";
+
+                mensagem = "Voce acertou uma letra!";
 
             }
 
-            //aumenta uma tentativa realizada
+            //Aumenta uma tentativa realizada
             tentativas++;
+
         }
 
         //Reinicia auxiliares
@@ -148,11 +166,24 @@ void jogarSozinho(){
     }
 
     if(palavra == palavraComMascara){
+
         limpaTela();
-        cout<<"YOU WIN!"<<endl;
+        cout << "Parabens, você venceu!";
+        cout << "\nDeseja reiniciar?";
+        cout << "\n1-Sim";
+        cout << "\n2-Nao";
+        cin >> opcao;
+        return opcao;
+
     }else{
+
         limpaTela();
-        cout<<"YOU LOOSE!"<<endl;
+        cout << "Bleh, você perdeu!";
+        cout << "\nDeseja reiniciar?";
+        cout << "\n1-Sim";
+        cout << "\n2-Nao";
+        cin >> opcao;
+        return opcao;
     }
 
 }
@@ -170,33 +201,49 @@ void menuInicial(){
         cout << "\n2 - Jogar em Dupla";
         cout << "\n3 - Sobre";
         cout << "\n4 - Sair";
-        cout << "\nEscolha uma opcao e tecle ENTER: ";
-        cin>>opcao;
+        cout << "\nEscolha uma opcao e tecle ENTER:";
 
-        //faz algo de acordo com a opcao escolhida
+        //Faz a leitura da opcao
+        cin >> opcao;
+
+        //Faz algo de acordo com a opcao escolhida
         switch(opcao){
+            case 1:
+                //Inicia o jogo
+                if(jogar(1) == 1){
+                    menuInicial();
+                }
+                break;
+            case 2:
+                //Inicia o jogo
+                if(jogar(2) == 1){
+                    menuInicial();
+                }
+                break;
+            case 3:
+                //Mostra informacoes do Jogo
+                cout << "Informacoes do jogo";
+                limpaTela();
+                cout << "Jogo desenvolvido por Joao em 2017";
+                cout << "\n1 - Voltar";
+                cout << "\n2 - Sair";
+                cin >> opcao;
+                if(opcao == 1){
+                    menuInicial();
+                }
 
-        case 1:
-            cout<<"\nJogo iniciado\n"<<endl;
-            jogarSozinho();
-            break;
-
-        case 2:
-            cout<<"\nInformacoes do jogo"<<endl;
-            break;
-
-        case 3:
-            cout<<"\nAte mais!"<<endl;
-            break;
-
+                break;
+            case 4:
+                cout << "Ate mais!";
+                break;
         }
     }
-}
 
+}
 
 int main(){
 
-
+    //Para gerar números realmente aleatórios
     srand( (unsigned)time(NULL));
 
     menuInicial();
