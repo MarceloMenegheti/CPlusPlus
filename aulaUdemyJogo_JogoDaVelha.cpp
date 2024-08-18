2c5ea711741b43a5f099fae9495765c0f26db8a9
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2024-08-17 23:18:06 -0300

    Máximo de Rodadas

--- aulaUdemyJogo_JogoDaVelha.cpp
+++ aulaUdemyJogo_JogoDaVelha.cpp
@@ -41,19 +41,18 @@ void exibeTabuleiro (char tabuleiro[3][3]){
 void jogo(string nomeDoJogadorUm, string nomeDoJogadorDois, int pontuacaoJogadorUm, int pontuacaoJogadorDois){
 
     ///Variáveis Gerais
-    //string nomeDoJogadorAtual;//Nomes dos jogadores
+    string nomeDoJogadorAtual;//Nomes dos jogadores
     char tabuleiro[3][3];                                       //Tabuleiro do Jogo
     int linha,coluna;                                           //Auxiliares para tabuleiro
     int linhaJogada,colunaJogada,posicaoJogada;                 //Posição em que o jogador posiciona sua marca
     int estadoDeJogo = 1;                                       //0 = Sem jogo,1 = Em Jogo
     int turnoDoJogador = 1;                                     //1 = X, 2 = O
+    int rodada = 0;                                             //Quantas vezes os jogadores jogaram no total
+    int opcao;                                                  //Opção de reinício
+    bool posicionouJogada;                                      //Verifica se o jogador colocou um marcador no tabuleiro
 
     //Coloca os '-' no tabuleiro para indicar o vazio
     iniciaTabuleiro(tabuleiro);
-
-
-    //Exibe o tabuleiro na tela
-    exibeTabuleiro(tabuleiro);
 
     while(rodada < 9 && estadoDeJogo == 1){
 
@@ -79,6 +78,76 @@ void jogo(string nomeDoJogadorUm, string nomeDoJogadorDois, int pontuacaoJogador
             nomeDoJogadorAtual = nomeDoJogadorDois;
 
         }
+
+        posicionouJogada = false;
+
+        //Matriz de posicoes possíveis
+        int posicoes[9][2] = {{2,0},{2,1},{2,2},{1,0},{1,1},{1,2},{0,0},{0,1},{0,2}};
+
+        while(posicionouJogada == false){
+
+            //Lê a jogada
+            cout << "\n" << nomeDoJogadorAtual << "Digite uma posicao conforme o mapa acima:";
+            cin >> posicaoJogada;
+
+            //Passa a linha e coluna de acordo com a matriz de posições exibida no mapa
+            linhaJogada = posicoes[posicaoJogada-1][0];
+            colunaJogada = posicoes[posicaoJogada-1][1];
+
+            //Verifica se a posição é vazia
+            if(tabuleiro[linhaJogada][colunaJogada] == '-'){
+
+                //Conseguiu posicionar um marcador
+                posicionouJogada = true;
+
+                //Verifica de quem é a vez para posicionar o marcador
+                if(turnoDoJogador == 1){
+
+                    tabuleiro[linhaJogada][colunaJogada] = 'X';
+
+                    turnoDoJogador = 2;
+
+                }else{
+
+                    tabuleiro[linhaJogada][colunaJogada] = 'O';
+
+                    turnoDoJogador = 1;
+
+                }
+
+            }
+
+        }
+
+        //Confere se o jogo acabou
+        if(confereTabuleiro(tabuleiro) == 1){
+            cout << "O jogador X venceu";
+            pontuacaoJogadorUm++;
+            estadoDeJogo = 0;
+        }else if(confereTabuleiro(tabuleiro) == 2){
+            cout << "O jogador O venceu";
+            pontuacaoJogadorDois++;
+            estadoDeJogo = 0;
+        }
+
+        //Aumenta uma rodada
+        rodada++;
+
+    }
+
+    exibeTabuleiro(tabuleiro);
+    cout << "Fim de jogo";
+    cout << "\nO que deseja fazer?";
+    cout << "\n1-Continuar Jogando";
+    cout << "\n2-Menu Inicial";
+    cout << "\n3-Sair";
+    cin >> opcao;
+    if(opcao == 1){
+        jogo(nomeDoJogadorUm, nomeDoJogadorDois,pontuacaoJogadorUm,pontuacaoJogadorDois);
+    }else if(opcao == 2){
+        menuInicial();
+    }
+
 }
 
 void menuInicial(){
