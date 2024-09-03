0463ceda27ad1e6dfef0b0a34d30dc93983d1fec
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2024-09-02 23:21:38 -0300

    Herança Exercicios - 3

--- aulaUdemyHerancaExericios.cpp
+++ aulaUdemyHerancaExericios.cpp
@@ -3,55 +3,76 @@
 using namespace std;
 
 //Classe Pai
-class Usuario{
+class Pessoa{
     private:
         string name;
-        string email;
+        string cpf;
 
     public:
         string getName(){return name;}
         void setName(string name){this->name = name;}
-        string getEmail(){return email;}
-        void setEmail(string email){this->email = email;}
+        string getCpf(){return cpf;}
+        void setCpf(string cpf){this->cpf = cpf;}
 };
 
 //Classe filha
-class Personagem : public Usuario{
+class Professor : public Pessoa{
     //Herda os métodos public e protected
-    //de Mamifero e tambem de Animal
-    private:
-        int nivel;
-    public:
-        int getNivel(){return nivel;}
-        void setNivel(int nivel){this->nivel = nivel;}
 
-        //Métodos diferentes costumam necessitar
-        //De comentários, a menis que o nome seja
-        //Autoexplicativo
-        void aumentaUmNivel(){
-            nivel += 1;
-            printf("Nivel aumentado %d",nivel);
+    private:
+        float salary;
+    public:
+        float getSalary(){return salary;}
+        void setSalary(float salary){this->salary = salary;}
+
+        //construtores
+        Professor(){}
+
+        Professor(float salary, string name, string cpf){
+            setSalary(salary);
+            setName(name);
+            setCpf(cpf);
         }
 };
 
 
 int main(){
 
-//instanciando o obj cachorro1 da Classe Cachorro
-Personagem personagem1;
+//instanciando o obj pessoa da Classe Pessoa
+Pessoa pessoa1;
 
 //passando os valores
-personagem1.setName("Marcelo");
-personagem1.setEmail("Mcelo@gmail.com");
-personagem1.setNivel(30);
+pessoa1.setName("Luiz");
+pessoa1.setCpf("19121123-12");
 
 //imprimindo os valores
-cout<<"Nome de usuario: " <<personagem1.getName()<<endl;
-cout<<"Email: " <<personagem1.getEmail()<<endl;
-cout<<"Nivel atual: " <<personagem1.getNivel()<<endl;
+cout<<"Nome: " <<pessoa1.getName()<<endl;
+cout<<"CPF: " <<pessoa1.getCpf()<<endl;
 
-//chamando o método
-personagem1.aumentaUmNivel();
+cout<<"\n-----\n";
+
+//instanciando o obj pfs da Classe Professor passando os valores
+Professor pf1(2988.48,"Marcelo","43321123-12");
+
+//imprimindo os valores
+cout<<"Nome: " <<pf1.getName()<<endl;
+cout<<"CPF: " <<pf1.getCpf()<<endl;
+cout<<"Salario: R$" <<pf1.getSalary()<<endl;
+
+cout<<"\n-----\n";
+
+//instanciando o obj pfs da Classe Professor
+Professor pf2;
+
+//passando os valores
+pf2.setName("John");
+pf2.setCpf("43300000-32");
+pf2.setSalary(4320.01);
+
+//imprimindo os valores
+cout<<"Nome: " <<pf2.getName()<<endl;
+cout<<"CPF: " <<pf2.getCpf()<<endl;
+cout<<"Salario: R$" <<pf2.getSalary()<<endl;
 
 return 0;
 }
